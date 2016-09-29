cc1b76b5a15321a8e42a0a38a4a2e56f2e907980
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-09-29 16:21:00 +0200

    add new matrix function

--- src/main/java/com/epam/tm/Main.java
+++ src/main/java/com/epam/tm/Main.java
@@ -3,10 +3,11 @@ package com.epam.tm;
 public class Main {
     public static void main(String[] args) {
 
+        int [][] a = new int[][]{{1,2,3},{4,5,6}};
 
-        Matrix m = new Matrix(5,5);
-        m.setRandom();
+        Matrix m = new Matrix(a);
         m.show();
+
 
     }
 }
--- src/main/java/com/epam/tm/Matrix.java
+++ src/main/java/com/epam/tm/Matrix.java
@@ -9,11 +9,17 @@ class Matrix {
     Matrix(int row,int col){
         this.row = row;
         this.col = col;
-        data = new int[row][col];
+        this.data = new int[row][col];
+        resetMatrix();
+    }
 
-        for (int i = 0;i < row;i++)
-            for (int j = 0;j < col;j++)
-                data[i][j] = 0;
+    Matrix(int[][] data){
+        if ((data.length > 0)  && (data[0].length > 0)){
+            this.row = data.length;
+            this.col = data[0].length;
+        }
+        this.data = new int[row][col];
+        setMatrix(data);
     }
 
     void setRandom(){
@@ -22,6 +28,19 @@ class Matrix {
                 data[i][j] = new Random().nextInt(1000);
     }
 
+    private void setMatrix(int[][] data){
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < col; j++)
+                    this.data[i][j] = data[i][j];
+    }
+
+    void resetMatrix(){
+        for (int i = 0;i < row;i++)
+            for (int j = 0;j < col;j++)
+                data[i][j] = 0;
+    }
+
+    //Вывод матрицы
     void show(){
         for (int i = 0;i < row;i++) {
             for (int j = 0; j < col; j++) {
